1e38e8986bb22fd08d6a9610841534020d10231c
Khang Tran <khangtrann8198@gmail.com>
2026-04-10 10:18:07 +0700

    feat: enhance recipe detail serialization and update viewset to support different serializers

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,3 +8,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ('id', 'title', 'time_minutes', 'price', 'description')
         read_only_fields = ('id',)
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ('description',)
