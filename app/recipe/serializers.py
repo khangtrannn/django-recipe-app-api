abd108dd78afeb3e545dd561de3b933b20dab423
Khang Tran <khangtrann8198@gmail.com>
2026-04-10 12:19:34 +0700

    feat: add Tag model support with serializers, views, and API endpoints

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects"""
@@ -21,3 +21,11 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ('description',)
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
