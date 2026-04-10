e83ae0792ef3ff832f95af5451e326980394e0b9
Khang Tran <khangtrann8198@gmail.com>
2026-04-10 17:42:50 +0700

    feat: enhance RecipeSerializer to manage tag creation and assignment during updates

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,17 +26,30 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ('id',)
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
-        for tag in tags:
-            tag_object, created = Tag.objects.get_or_create(
-                user=self.context['request'].user,
-                **tag,
-            )
-            recipe.tags.add(tag_object)
-
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
